bf19767648ca02232989a841ea0db7134abef1eb
lschaeff <leon.schaeffer@me.com>
2021-07-17 12:33:51 +0200

    refactor con_tab

--- lib/knx/knxnetip/con_tab.ex
+++ lib/knx/knxnetip/con_tab.ex
@@ -2,61 +2,70 @@ defmodule Knx.KnxnetIp.ConTab do
   alias Knx.KnxnetIp.Connection, as: C
   alias Knx.KnxnetIp.IpFrame
 
-  def open(con_tab, con_type, %IpFrame{} = ip_frame) do
-    # add once, when first connection is opened
-    con_tab = Map.put_new(con_tab, :free_mgmt_ids, Enum.to_list(0..254))
-    free_mgmt_ids = con_tab[:free_mgmt_ids]
+  def open(%{tunnel_cons_left: 0}, :tunnel_con, _ip_frame) do
+    {:error, :no_more_connections}
+  end
 
-    new_connection = %C{
-      con_type: con_type,
-      dest_control_endpoint: ip_frame.control_endpoint,
-      dest_data_endpoint: ip_frame.data_endpoint,
-      client_seq_counter: 0,
-      server_seq_counter: 0
-    }
+  def open(%{tunnel_cons_left: tunnel_cons_left} = con_tab, con_type, %IpFrame{} = ip_frame) do
+    case List.pop_at(con_tab[:free_ids], 0) do
+      {nil, _} ->
+        {:error, :no_more_connections}
 
-    case con_type do
-      # allows only one open tunnelling connection (always has id 255)
-      # TODO allow multiple tunnelling connections ?
-      :tunnel_con ->
-        if is_open?(con_tab, 255) do
-          {:error, :no_more_connections}
-        else
-          con_tab = Map.put_new(con_tab, 255, %C{new_connection | id: 255})
-          {:ok, con_tab, 255}
-        end
+      {free_id, free_ids} ->
+        new_connection = %C{
+          id: free_id,
+          con_type: con_type,
+          dest_control_endpoint: ip_frame.control_endpoint,
+          dest_data_endpoint: ip_frame.data_endpoint,
+          client_seq_counter: 0,
+          server_seq_counter: 0
+        }
 
-      :device_mgmt_con ->
-        case List.pop_at(free_mgmt_ids, 0) do
-          {nil, _free_mgmt_ids} ->
-            {:error, :no_more_connections}
+        con_tab = Map.put_new(con_tab, free_id, new_connection)
+        con_tab = put_in(con_tab[:free_ids], free_ids)
 
-          {new_id, free_mgmt_ids} ->
-            con_tab = Map.put_new(con_tab, new_id, %C{new_connection | id: new_id})
-            con_tab = put_in(con_tab[:free_mgmt_ids], free_mgmt_ids)
-            {:ok, con_tab, new_id}
-        end
+        con_tab =
+          if con_type == :tunnel_con do
+            %{con_tab | tunnel_cons_left: tunnel_cons_left - 1}
+          else
+            con_tab
+          end
+
+        {:ok, con_tab, free_id}
+    end
+  end
+
+  def open(%{} = con_tab, con_type, %IpFrame{} = ip_frame) do
+    # initialize map on first call
+    con_tab = Map.put_new(con_tab, :free_ids, Enum.to_list(0..255))
+    # limit number of simultaneous tunnelling connections
+    con_tab = Map.put_new(con_tab, :tunnel_cons_left, 1)
+
+    open(con_tab, con_type, ip_frame)
+  end
+
+  def close(con_tab, id) do
+    case Map.pop(con_tab, id) do
+      {nil, _} ->
+        {:error, :connection_id}
+
+      {%C{con_type: con_type}, %{tunnel_cons_left: tunnel_cons_left} = con_tab} ->
+        con_tab = Map.put(con_tab, :free_ids, [id | con_tab[:free_ids]])
+
+        con_tab =
+          if con_type == :tunnel_con do
+            %{con_tab | tunnel_cons_left: tunnel_cons_left + 1}
+          else
+            con_tab
+          end
+
+        {:ok, con_tab}
     end
   end
 
   def is_open?(con_tab, id) do
     Map.has_key?(con_tab, id)
   end
-
-  # !info: is this necessary? without checking the source data ep, knowing the
-  #  channel id (only applies for device management)
-  #  and sequence counter is enough to hijack the connection
-  # def check_access(con_tab, id, src_data_endpoint) do
-  #   if Map.has_key?(con_tab, id) do
-  #     if con_tab[id].dest_data_endpoint == src_data_endpoint do
-  #       :ok
-  #     else
-  #       {:error, :access_forbidden}
-  #     end
-  #   else
-  #     {:error, :connection_id}
-  #   end
-  # end
 
   def increment_client_seq_counter(con_tab, id) do
     cur_count = con_tab[id].client_seq_counter
@@ -77,8 +86,10 @@ defmodule Knx.KnxnetIp.ConTab do
     cond do
       counter == con_tab[id].client_seq_counter ->
         :counter_equal
+
       incremented_counter == con_tab[id].client_seq_counter ->
         :counter_off_by_minus_one
+
       true ->
         :any_other_case
     end
@@ -106,19 +117,5 @@ defmodule Knx.KnxnetIp.ConTab do
 
   def get_data_endpoint(con_tab, id) do
     con_tab[id].dest_data_endpoint
-  end
-
-  def close(con_tab, id) do
-    case Map.pop(con_tab, id) do
-      {nil, _con_tab} ->
-        {:error, :connection_id}
-
-      {_con, con_tab} ->
-        if id == 255 do
-          {:ok, con_tab}
-        else
-          {:ok, Map.put(con_tab, :free_mgmt_ids, [id | con_tab[:free_mgmt_ids]])}
-        end
-    end
   end
 end
--- test/knx/knxnetip/con_tab_test.exs
+++ test/knx/knxnetip/con_tab_test.exs
@@ -9,55 +9,67 @@ defmodule Knx.KnxnetIp.ConTabTest do
   @control_endpoint %Ep{ip_addr: 0xC0A8_B23E, port: 0x0E75}
   @data_endpoint %Ep{ip_addr: 0xC0A8_B23E, port: 0x0E76}
 
-  @list_0_254 Enum.to_list(0..254)
-  @list_1_254 Enum.to_list(1..254)
+  @list_0_255 Enum.to_list(0..255)
+  @list_1_255 Enum.to_list(1..255)
+  @list_2_255 Enum.to_list(2..255)
 
-  @con_0 %C{
+  @con_mgmt %C{
     id: 0,
     con_type: :device_mgmt_con,
     dest_control_endpoint: @control_endpoint,
-    dest_data_endpoint: @data_endpoint
+    dest_data_endpoint: @data_endpoint,
+    client_seq_counter: 0,
+    server_seq_counter: 0
   }
 
-  @con_255 %C{
-    id: 255,
+  @con_tunnel %C{
+    id: 1,
     con_type: :tunnel_con,
     dest_control_endpoint: @control_endpoint,
-    dest_data_endpoint: @data_endpoint
+    dest_data_endpoint: @data_endpoint,
+    client_seq_counter: 0,
+    server_seq_counter: 0
   }
 
   @con_tab_0 %{
-    :free_mgmt_ids => @list_1_254,
-    0 => @con_0
+    :free_ids => @list_1_255,
+    :tunnel_cons_left => 1,
+    0 => @con_mgmt
   }
 
-  @con_tab_255 %{
-    :free_mgmt_ids => @list_0_254,
-    255 => @con_255
+  @con_tab_1 %{
+    :free_ids => @list_2_255,
+    :tunnel_cons_left => 0,
+    0 => @con_mgmt,
+    1 => @con_tunnel
   }
 
   @con_tab_0_client_seq_1 %{
-    :free_mgmt_ids => @list_1_254,
-    0 => %C{@con_0 | client_seq_counter: 1}
+    :free_ids => @list_1_255,
+    :tunnel_cons_left => 1,
+    0 => %C{@con_mgmt | client_seq_counter: 1}
   }
 
   @con_tab_0_client_seq_255 %{
-    :free_mgmt_ids => @list_1_254,
-    0 => %C{@con_0 | client_seq_counter: 255}
+    :free_ids => @list_1_255,
+    :tunnel_cons_left => 1,
+    0 => %C{@con_mgmt | client_seq_counter: 255}
   }
 
   @con_tab_0_server_seq_1 %{
-    :free_mgmt_ids => @list_1_254,
-    0 => %C{@con_0 | server_seq_counter: 1}
+    :free_ids => @list_1_255,
+    :tunnel_cons_left => 1,
+    0 => %C{@con_mgmt | server_seq_counter: 1}
   }
 
   @con_tab_0_server_seq_255 %{
-    :free_mgmt_ids => @list_1_254,
-    0 => %C{@con_0 | server_seq_counter: 255}
+    :free_ids => @list_1_255,
+    :tunnel_cons_left => 1,
+    0 => %C{@con_mgmt | server_seq_counter: 255}
   }
 
   test "open: device management connection" do
-    assert {:ok, %{:free_mgmt_ids => @list_1_254, 0 => @con_0}, 0} =
+    assert {:ok, @con_tab_0, 0} =
              ConTab.open(
                %{},
                :device_mgmt_con,
@@ -69,7 +81,7 @@ defmodule Knx.KnxnetIp.ConTabTest do
 
     assert {:error, :no_more_connections} =
              ConTab.open(
-               %{:free_mgmt_ids => []},
+               %{:free_ids => []},
                :device_mgmt_con,
                %IpFrame{
                  control_endpoint: @control_endpoint,
@@ -79,9 +91,9 @@ defmodule Knx.KnxnetIp.ConTabTest do
   end
 
   test "open: tunneling connection" do
-    assert {:ok, %{:free_mgmt_ids => @list_0_254, 255 => @con_255}, 255} =
+    assert {:ok, @con_tab_1, 1} =
              ConTab.open(
-               %{},
+               @con_tab_0,
                :tunnel_con,
                %IpFrame{
                  control_endpoint: @control_endpoint,
@@ -91,13 +103,19 @@ defmodule Knx.KnxnetIp.ConTabTest do
 
     assert {:error, :no_more_connections} =
              ConTab.open(
-               %{:free_mgmt_ids => @list_0_254, 255 => @con_255},
+               @con_tab_1,
                :tunnel_con,
                %IpFrame{
                  control_endpoint: @control_endpoint,
                  data_endpoint: @data_endpoint
                }
              )
+  end
+
+  test "close connection" do
+    assert {:error, :connection_id} = ConTab.close(@con_tab_0, 5)
+    assert {:ok, %{:free_ids => @list_0_255}} = ConTab.close(@con_tab_0, 0)
+    assert {:ok, @con_tab_0} = ConTab.close(@con_tab_1, 1)
   end
 
   test "is connection open?" do
@@ -135,11 +153,5 @@ defmodule Knx.KnxnetIp.ConTabTest do
   test "get endpoint" do
     assert @control_endpoint = ConTab.get_control_endpoint(@con_tab_0, 0)
     assert @data_endpoint = ConTab.get_data_endpoint(@con_tab_0, 0)
-  end
-
-  test "close connection" do
-    assert {:error, :connection_id} = ConTab.close(@con_tab_0, 5)
-    assert {:ok, %{:free_mgmt_ids => @list_0_254}} = ConTab.close(@con_tab_0, 0)
-    assert {:ok, %{:free_mgmt_ids => @list_0_254}} = ConTab.close(@con_tab_255, 255)
   end
 end
